b1fe125ad8b142482a42b2193cbff5af3f3d7abf
rey-vthi <108cs16047@gmail.com>
2020-10-15 16:48:00 +0530

    made build

---
+++ public/precache-manifest.8944024bef91e3f7cf400e5eaa0bbf27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c95390b9255ab0a508248bfd4f7d5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce34a9742800ecdb41cc",
+    "url": "/static/js/2.b5b0fc10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5b0fc10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0501a799523f6c6096d",
+    "url": "/static/js/main.7f635e01.chunk.js"
+  },
+  {
+    "revision": "100658504a6d4965b7b5",
+    "url": "/static/js/runtime-main.faf8911e.js"
+  }
+]);
